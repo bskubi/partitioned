296db78d75a97278fccd71fe4eed42ba9939a5d4
Ben Skubi <bskubi@gmail.com>
2024-11-05 13:30:54 -0800

    Updates to README and setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     description='Determine if a series of lines is partitioned (all identical lines sequential).',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/bskubi/partitioned',  # Update this if applicable
+    url='https://github.com/bskubi/partitioned',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
