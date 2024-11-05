3e79b48945eefd38a08833198fcab4491491b19f
Ben Skubi <bskubi@gmail.com>
2024-11-05 13:40:33 -0800

    Patch update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='partitioned',
-    version='0.1.1',
+    version='0.1.2',
     py_modules=['partitioned'],
     author='Benjamin Skubi',
     author_email='skubi@ohsu.edu',
